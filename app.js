faef5287ce91249d4ff6e66b3e3d2b9c2c515769
mathan Lakshmanan <63947974+mathanlakshmanan@users.noreply.github.com>
2023-03-28 14:03:11 +0530

    modify

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const express = require('express');
 const userRoute = require('./routes/userRoute');
 const app = express();
 require("dotenv").config(); 
+var cors = require('cors')
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(cors());
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb+srv://mathankumar:Mathanlakma5@cluster0.gdlv0c3.mongodb.net/staffmanage",
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://mathankumar:Mathanlakma5@cluster0.gdlv0c3.mongodb.net/staffmanage",
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 },(err)=>{
     if(err){
-        console.log("here");
         console.log(err);
         
     }else{
@@ -23,6 +24,6 @@ mongoose.connect("mongodb+srv://mathankumar:Mathanlakma5@cluster0.gdlv0c3.mongod
 });
 
 app.use("/api/user", userRoute);
-app.listen(3000, ()=>{
-    console.log(`server running successfully on port : 3000`);
+app.listen(process.env.PORT, ()=>{
+    console.log(`server running successfully on port : ${process.env.PORT}`);
 })
